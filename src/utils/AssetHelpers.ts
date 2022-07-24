6404659a69a73dd1256be6d917a37a1aef71daa8
Ali Bidjandy <ali.bidjandy@gmail.com>
2022-07-24 13:09:25 +0430

    second commit

--- src/utils/AssetHelpers.ts
+++ src/utils/AssetHelpers.ts
@@ -1,2 +1,7 @@
-export const toAbsoluteUrl = (pathname: string) =>
-  process.env.PUBLIC_URL + pathname;
+export const toAbsoluteUrl = (pathname: string) => {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    return process.env.PUBLIC_URL + pathname;
+  } else {
+    return window.location.hostname + pathname;
+  }
+};
